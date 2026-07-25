830b6a1fc65234aaf07c1c2f0251f83de2d8a792
Pravesh Yadav <praveshyadav6307@gmail.com>
2026-07-25 21:49:35 +0530

    Fixed

--- index.js
+++ index.js
@@ -589,7 +589,7 @@ client.on('interactionCreate', async (interaction) => {
 
     if (spinType === 'spin_giveaway_1') {
       //secretWinners = []; 
-      secretWinners = ['1207494441145864302', '1456333793286950952', '747294530264498287', '1530594748434223162'];
+      secretWinners = ['1207494441145864302', '1456333793286950952', '747294530264498287', '1530594748434223162', '1530606961417130056'];
     } else if (spinType === 'spin_giveaway_2') {
       //secretWinners = ['1500483808296828930', '1523350653584277584', '1170694062953726093', '1517521853143453817']; 
       secretWinners = [];
